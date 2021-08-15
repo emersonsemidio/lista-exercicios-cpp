e75b58f30acf722892f4611335eb91cf03d2af13
Emerson Emidio <emersons.emidio01@gmail.com>
2021-08-15 16:15:22 -0300

    Lista III - Exercicio 11

---
+++ lista03_E11.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int n, m, n1, m1;
+    printf("N =");
+    scanf("%d", &n);
+    printf("M =");
+    scanf("%d", &m);
+    if(n>m){
+        m1 = n;
+        n1 = m;
+        m = m1;
+        n = n1;
+    }
+    puts("Os impares no intervalo sao: ");
+    for(n;n<=m;n++){
+        if(n%2==1){
+            printf("%d\n", n);
+
+        }
+    }
+}
