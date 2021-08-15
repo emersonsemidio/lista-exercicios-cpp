8a5b680d8871a2f1760acf2a3c05ed1c79ecf308
Emerson Emidio <emersons.emidio01@gmail.com>
2021-08-15 16:16:03 -0300

    Lista IV - Exercicios 1 e 2

---
+++ lista04_E01.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+int i = 0, v[10];
+
+
+for(i; i<10; i++){
+printf("V = ");
+scanf("%d", &v[i]);
+}
+i=0;
+
+for(i; i<10; i++){
+    printf("%.0f\n", pow(v[i], 2));
+
+}
+}
+
+
+
