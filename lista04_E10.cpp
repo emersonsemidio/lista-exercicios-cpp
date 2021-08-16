73adb3efe0f5dc386d9c080c7631270d412c1de6
Emerson Emidio <emersons.emidio01@gmail.com>
2021-08-15 23:45:31 -0300

    Utilizando constante TAM_VETOR

--- lista04_E10.cpp
+++ lista04_E10.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main(){
     const int TAM_VETOR=5;
-    int v[5], w[5], k[10], i;
+    int v[TAM_VETOR], w[TAM_VETOR], k[TAM_VETOR*2], i;
 
-    for(i=0;i<=4;i++){
+    for(i=0;i<TAM_VETOR;i++){
         printf("V = ");
         scanf("%d", &v[i]);
     }
 
-    for(i=0;i<=4;i++){
+    for(i=0;i<TAM_VETOR;i++){
         printf("W = ");
         scanf("%d", &w[i]);
     }
 
-    for(i=0;i<=9;i++){
+    for(i=0;i<TAM_VETOR*2;i++){
         if(i<=4){
             k[i] = v[i];
         }
@@ -22,7 +22,7 @@ int main(){
         }
     }
 
-    for(i=0;i<=9;i++){
+    for(i=0;i<TAM_VETOR*2;i++){
         if(i==0){
             puts("Os valores de K sao: ");
         }
