b9058306cecf7870b784ca43e75a4cd15cf843cc
Emerson Emidio <emersons.emidio01@gmail.com>
2021-08-25 07:59:25 -0300

    V3

--- frequencia.cpp
+++ frequencia.cpp
@@ -11,12 +11,12 @@ int main(){
     putchar('\n');
 
     for( i=0;i<n;i++){
-        printf("V = ");
+        printf("V[%d] = ", i);
         scanf("%lf", &num);
         j = 0;
         achou = false;
 
-        while (achou == false && j < i) {
+        while (achou == false && j < k) {
             if (v[j] == num) {
                 achou = true;
                 cont[j]++;
@@ -29,14 +29,19 @@ int main(){
             k++;
             v[k] = num;
             cont[k]=1;
+
+            printf("k=%d | i=%d - ", k,i);
+            for(int tmp = 0; tmp < k; tmp++) {
+                printf("%.1lf ", v[tmp]);
+            }
+            printf("\n\n\n");
         }
     }
+
+    printf("fim teste\n");
 
 
     for(i=0;i<k;i++){
         printf("%.1f apareceu %.1f vezes\n", v[i], cont[i]);
     }
-
-
-
 }
