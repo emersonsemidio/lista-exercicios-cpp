ff637fdce684da898ec31528b93014cd7cce207d
Emerson Emidio <emersons.emidio01@gmail.com>
2021-08-25 07:05:37 -0300

    Teste Frequencia

--- frequencia.cpp
+++ frequencia.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int n=0, c=0, qtd_numeros=1;
+    int n=0, c=0, qtd_numeros=1, i = 0, j = 0;
     double v[n], cont[c];
 
     do{
@@ -9,20 +9,30 @@ int main(){
     }while(n<=0);
     putchar('\n');
 
-    for(int i=0;i<n;i++){
+    for( i=0;i<n;i++){
         printf("V = ");
         scanf("%lf", &v[i]);
     }
 
-    for(int i=0;i<n;i++){
+    for( i=0;i<n;i++){
+        printf("%lf ", v[i]);
+    }
+    printf("\n\n\n");
 
-        for(int j=0; j<n; j++){
-            qtd_numeros = 1;
+    for(i=0;i<n;i++){
+        for( int tmp =0;tmp<n;tmp++){
+            printf("[%d] %lf ", i, v[tmp]);
+        }
+        printf("\n\n");
+
+        qtd_numeros = 0;
+        for(j=0; j<n; j++){
             if(v[i]!=v[j]){
-                cont[c] = qtd_numeros;
+                // cont[c] = qtd_numeros;
             }
             if(v[i]==v[j]){
                 qtd_numeros = qtd_numeros + 1;
+                //cont[c] = qtd_numeros;
             }
             if(j==n-1){
                 c++;
@@ -32,8 +42,13 @@ int main(){
 
     }
 
-    for(int i=0;i<n;i++){
-        printf("%f %f vezes\n", v[i], cont[i]);
+    for( i=0;i<n;i++){
+        printf("%lf ", v[i]);
+    }
+    printf("\n\n\n");
+
+    for(i=0;i<n;i++){
+        printf("%.1f apareceu %.1f vezes\n", v[i], cont[i]);
     }
 
 
