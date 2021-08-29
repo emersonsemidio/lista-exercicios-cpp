c38ee96b23bad5c88c0979b38c8fc31273fa8bd5
Emerson Emidio <emersons.emidio01@gmail.com>
2021-08-29 11:19:32 -0300

    Final Frequencia

--- frequencia.cpp
+++ frequencia.cpp
@@ -2,7 +2,7 @@
 int main(){
     int n=0, c=0, qtd_numeros=1, i = 0, j = 0, k = -1;
     bool achou = false;
-    int v[n], cont[c], num;
+    double num;
 
     do{
         printf("N = ");
@@ -10,10 +10,12 @@ int main(){
     }while(n<=0);
     putchar('\n');
 
+    double v[n], cont[n];
+
     k = -1;
     for(i = 0; i < n; i++) {
-        printf("V[%d] = ", i);
-        scanf("%d", &num);
+        printf("V = ");
+        scanf("%lf", &num);
         achou = false;
         j = 0;
 
@@ -21,7 +23,7 @@ int main(){
             if (v[j] == num) {
                 achou = true;
                 cont[j] = cont[j]+1;
-                printf("v[%d] == %d\n", j, num);
+                printf("v[%d] == %.1lf\n", j, num);
             } else {
                 j = j+1;
             }
@@ -32,13 +34,11 @@ int main(){
             k = k+1;
             v[k]= num;
             cont[k]=1;
-            printf("achou == false. v[%d]=%d e cont[%d]=1\n",k, num, k);
-        } else {
-            printf("achou == true. v[%d]=%d e cont[%d]=1\n",k, num, k);
         }
+
     }
 
     for (j = 0; j<= k; j++) {
-        printf("%d apareceu %d\n", v[j], cont[j]);
+        printf("%.1lf apareceu %.1lf\n", v[j], cont[j]);
     }
 }
